912cf19ddd4278d7564858e278121a7b567f9bce
ter0n <tyarin.petr@gmail.com>
2020-01-15 20:18:50 +0300

    Add deletion
    Add deletion of the task list item

--- app/src/main/java/com/example/lifeprogress/MainActivity.kt
+++ app/src/main/java/com/example/lifeprogress/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.lifeprogress
 
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.ContextMenu.ContextMenuInfo
+import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView.AdapterContextMenuInfo
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
+
+private const val CM_DELETE_ID = 1 //номер кнопки "Удалить" в контекстном меню
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,26 +21,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val taskList = listOf(
-//            DailyTask("task1", true),
-//            DailyTask("task2", false),
-//            DailyTask("ta sk3", true),
-//            DailyTask("task4", true),
-//            DailyTask("task5", false)
-//        )
-
         taskList.add(DailyTask("task1", true))
         taskList.add(DailyTask("task2", false))
         taskList.add(DailyTask("task3", true))
         taskList.add(DailyTask("task4", true))
         taskList.add(DailyTask("task5", false))
 
-//        val lvTaskAdapter = LVTasksAdapter(this, taskList)
         lvTaskAdapter = LVTasksAdapter(this, taskList)
         lv_tasks.adapter = lvTaskAdapter
-        taskList.add(DailyTask("task6", false))
-        lvTaskAdapter.notifyDataSetChanged()
-
+        registerForContextMenu(lv_tasks) //прикрепляем контекстное меню
     }
 
     fun addLVItem (veiw: View){
@@ -42,8 +38,20 @@ class MainActivity : AppCompatActivity() {
         lvTaskAdapter.notifyDataSetChanged()
     }
 
-    fun removeLVItem (veiw: View){
+    override fun onCreateContextMenu(menu : ContextMenu, v : View, menuInfo : ContextMenuInfo){
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menu.add(0, CM_DELETE_ID, 0, "Удалить")
+    }
 
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == CM_DELETE_ID){
+            //получаем информацию о пункте списка
+            val acmi  = item.menuInfo as AdapterContextMenuInfo
+            taskList.removeAt(acmi.position)
+            lvTaskAdapter.notifyDataSetChanged()
+            return true
+        }
+        return super.onContextItemSelected(item)
     }
 
 }
